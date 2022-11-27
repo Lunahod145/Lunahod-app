dc7916e2304bcbc7c8d4d285535b4d17ca6e2ba3
LEv145 <you@example.com>
2022-11-27 13:19:25 +0200

    Add many codes for event

--- src/lunohod_app/api/joysticks/base.py
+++ src/lunohod_app/api/joysticks/base.py
@@ -1,3 +1,4 @@
+import typing as t
 import abc
 from threading import Thread
 from dataclasses import dataclass
@@ -9,12 +10,12 @@ from inputs import GamePad, InputEvent
 class Joystick(abc.ABC):
     def __init__(self, joystick: GamePad) -> None:
         self._joystick = joystick
-        self._events: dict[str, "JoystickEvent"] = {}
+        self._event_tuples: list[EventTuple] = []
 
         self._start_threads()
 
-    def set_event(self, code: str, event: "JoystickEvent"):
-        self._events[code] = event
+    def add_event(self, event_tuple: "EventTuple"):
+        self._event_tuples.append(event_tuple)
 
     def _start_threads(self) -> None:
         self._monitor_thread = Thread(target=self._controller_thread, daemon=True)
@@ -22,28 +23,40 @@ class Joystick(abc.ABC):
 
     def _controller_thread(self):
         while True:
-            joystick_events = self._joystick.read()
-            for joystick_event in joystick_events:
+            for joystick_event in self._joystick.read():
                 if not(joystick_event.ev_type in ("Absolute", "Key")):
                     continue
-                try:
-                    event = self._events[joystick_event.code]
-                except KeyError:
-                    continue
 
-                joystick_event = self._prepare_joystick_event(joystick_event)
-                event.do(joystick_event)
+                self._start_events_by_joystick_event(joystick_event)
 
     @abc.abstractmethod
     def _prepare_joystick_event(self, joystick_event: InputEvent) -> "JoystickInput":
         pass
 
+    def _start_events_by_joystick_event(self, joystick_event: InputEvent) -> None:
+        for event in self._filter_events_by_code(joystick_event.code):
+            event.do(event=self._prepare_joystick_event(joystick_event))
+
+    def _filter_events_by_code(self, code: str) -> list["JoystickEvent"]:
+        return [
+            event_tuple.event
+            for event_tuple in self._event_tuples
+            if code in event_tuple.codes
+        ]
+
 
 class JoystickEvent(abc.ABC):
-    @abc.abstractmethod
     def do(self, event: "JoystickInput") -> None:
+        if event.state != 0:
+            self.on_pressed(event)
+        else:
+            self.on_release(event)
+
+    def on_pressed(self, event: "JoystickInput"):
         pass
 
+    def on_release(self, event: "JoystickInput"):
+        pass
 
 @dataclass
 class JoystickInput():
@@ -51,3 +64,8 @@ class JoystickInput():
     timestamp: datetime
     code: str
     state: float
+
+
+class EventTuple(t.NamedTuple):
+    codes: list[str]
+    event: "JoystickEvent"
