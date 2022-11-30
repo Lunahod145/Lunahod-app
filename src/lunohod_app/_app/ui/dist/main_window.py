0970e51019477f1acb92bf8d6436397f68219b8b
LEv145 <you@example.com>
2022-12-01 00:09:00 +0200

    v 0.1
    Update tests
    Fix xbox36 module for stick and triggers
    New (more) modular architecture
    Update python version

--- src/lunohod_app/_app/ui/dist/main_window.py
+++ src/lunohod_app/_app/ui/dist/main_window.py
@@ -8,15 +8,15 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide2.QtMultimediaWidgets import QVideoWidget
-from PySide2.QtWidgets import (QApplication, QGridLayout, QMainWindow, QSizePolicy,
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QSizePolicy,
     QWidget)
 
 class Ui_MainWindow(object):
@@ -45,3 +45,4 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
     # retranslateUi
+
